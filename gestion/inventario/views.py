1b919689f4e47114dd5f3fbaf146144b2ddd585e
AntonioPromaweb <vasquezfloresluisantonio33@gmail.com>
2026-09-02 18:31:05 -0500

    Implente Create

--- gestion/inventario/views.py
+++ gestion/inventario/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Producto
+from .forms import ProductoForm
 
-# Create your views here.
+# Vista para Listar Productos (Destino del redirect)
+def lista_productos(request):
+    productos = Producto.objects.all()
+    return render(request, 'inventario/lista_productos.html', {'productos': productos})
+
+# Vista para Crear un Producto (CREATE mediante ORM)
+def crear_producto(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()  # Guarda en SQLite usando Django ORM
+            return redirect('lista_productos')  # Redirige al listado
+    else:
+        form = ProductoForm()
+    
+    return render(request, 'inventario/crear_producto.html', {'form': form})
